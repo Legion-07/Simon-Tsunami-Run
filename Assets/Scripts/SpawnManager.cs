be7d92ef904b1bbe5e9d799305c57cd097fa8a86
ElijahColeCradic <elicradic+DB@gmail.com>
2025-11-13 11:52:39 -0500

    I added various things, including a spawnmanager and movement script.

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] ObstaclePrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    private float startDelay = 2f;
+    private float spawnInterval = 1.5f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomObstacle", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void SpawnRandomObstacle()
+    {
+        int ObstacleIndex = Random.Range(0, ObstaclePrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(ObstaclePrefabs[ObstacleIndex], spawnPos, ObstaclePrefabs[ObstacleIndex].transform.rotation);
+    }
+    }
