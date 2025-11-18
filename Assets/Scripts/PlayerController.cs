b9c959b7b889ec1364370111d780253e21a9fb9b
franyzimmerman <githubfranyzimmerman@gmail.com>
2025-11-18 11:42:21 -0500

    new stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -33,6 +33,7 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
         }
+    
     }
 
     void FixedUpdate()
